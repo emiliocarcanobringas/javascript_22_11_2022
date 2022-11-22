f164e3621fe811278e0177e598dbd783c28f9dd8
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2022-11-22 00:27:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Hello World')
+
+const place = "World"
+const greeting = "Hello"
+
+
+console.log(`${greeting} ${place}`);
+
+console.log("Hello, infinite and beyond");
+
+//VS
+
+// Este es un comentario
+
+/* A funcion could be auto documented because the function name explain what the function do
+
+function reverseString(value){
+let reversedValue =  "";
+
+value.split("").forEach((char) =>{
+    reversedValue = char + reversedValue;
+});
+
+return recersedValue;
+
+}
+
+console.log(reverseString("Reverse Me"));
+
+*/
+
